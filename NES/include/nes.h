d1ac13a68c9b63f29555b270c0b5bd6db87ef385
root <root@DESKTOP-LNPFT64.localdomain>
2018-07-22 23:38:09 +0200

    opcodes changed to match addressing modes

--- NES/include/nes.h
+++ NES/include/nes.h
@@ -7,241 +7,199 @@ typedef uint8_t byte_t;
 /*
  * OPCODE FORMAT:
  * ==============
- * OPCODE_ADDRESSINGMODE(_REGISTER)
+ * ADDRESSINGMODE_OPCODE
+ *
+ * ADDRESSING MODES:
+ * =================
+ * IMM      => Immediate
+ * ZP       => Zero-Page
+ * ABS      => Absolute
+ * IMP      => Implied
+ * ACC      => Accumulator
+ * IXX      => Indexed (index register X)
+ * IXY      => Indexed (index register Y)
+ * ZPIXX    => Zero-Page Indexed (index register X)
+ * ZPIXY    => Zero-Page Indexed (index register Y)
+ * IDR      => Indirect
+ * PREII    => Pre-Indexed Indirect
+ * POSII    => Post-Indexed Indirect
+ * REL      => Relative
  *
  * OPCODES:
  * ========
  *
  *
- * ADDRESSING MODES:
- * =================
- * IMM => Immediate
- * ZP  => Zero-Page
- * AB  => Absolute
- * IMP => Implied
- * AC  => Accumulator
- * IX  => Indexed
- * ZPI => Zero-Page Indexed
- * IR  => Indirect
- * PRE => Pre-Indexed Indirect
- * POS => Post-Indexed Indirect
- * RE  => Relative
  *
- * REGISTER:
- * =========
- * X
- * Y
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 
-#define ADC_IMM         ((byte_t) 0x69)
-#define ADC_ZP          ((byte_t) 0x65)
-#define ADC_ZP_X        ((byte_t) 0x75)
-#define ADC_AB          ((byte_t) 0x6D)
-#define ADC_AB_X        ((byte_t) 0x7D)
-#define ADC_AB_Y        ((byte_t) 0x79)
-#define ADC_IR_X        ((byte_t) 0x61)
-#define ADC_IR_Y        ((byte_t) 0x71)
+#define IMM_ADC         ((byte_t) 0x69)
+#define IMM_AND         ((byte_t) 0x29)
+#define IMM_CMP         ((byte_t) 0xC9)
+#define IMM_CPX         ((byte_t) 0xE0)
+#define IMM_CPY         ((byte_t) 0xC0)
+#define IMM_EOR         ((byte_t) 0x49)
+#define IMM_LDA         ((byte_t) 0xA9)
+#define IMM_LDX         ((byte_t) 0xA2)
+#define IMM_LDY         ((byte_t) 0xA0)
+#define IMM_ORA         ((byte_t) 0x09)
+#define IMM_SBC         ((byte_t) 0xE9)
 
-#define AND_IMM         ((byte_t) 0x29)
-#define AND_ZP          ((byte_t) 0x25)
-#define AND_ZP_X        ((byte_t) 0x35)
-#define AND_AB          ((byte_t) 0x2D)
-#define AND_AB_X        ((byte_t) 0x3D)
-#define AND_AB_Y        ((byte_t) 0x39)
-#define AND_IR_X        ((byte_t) 0x21)
-#define AND_IR_Y        ((byte_t) 0x31)
+#define ZP_ADC          ((byte_t) 0x65)
+#define ZP_AND          ((byte_t) 0x25)
+#define ZP_ASL          ((byte_t) 0x06)
+#define ZP_BIT          ((byte_t) 0x24)
+#define ZP_CMP          ((byte_t) 0xC5)
+#define ZP_CPX          ((byte_t) 0xE4)
+#define ZP_CPY          ((byte_t) 0xC4)
+#define ZP_DEC          ((byte_t) 0xC6)
+#define ZP_EOR          ((byte_t) 0x45)
+#define ZP_INC          ((byte_t) 0xE6)
+#define ZP_LDA          ((byte_t) 0xA5)
+#define ZP_LDX          ((byte_t) 0xA6)
+#define ZP_LDY          ((byte_t) 0xA4)
+#define ZP_LSR          ((byte_t) 0x46)
+#define ZP_ORA          ((byte_t) 0x05)
+#define ZP_ROL          ((byte_t) 0x26)
+#define ZP_ROR          ((byte_t) 0x66)
+#define ZP_SBC          ((byte_t) 0xE5)
+#define ZP_STA          ((byte_t) 0x85)
+#define ZP_STX          ((byte_t) 0x86)
+#define ZP_STY          ((byte_t) 0x84)
 
-#define ASL_AC          ((byte_t) 0x0A)
-#define ASL_ZP          ((byte_t) 0x06)
-#define ASL_ZP_X        ((byte_t) 0x16)
-#define ASL_AB          ((byte_t) 0x0E)
-#define ASL_AB_X        ((byte_t) 0x1E)
+#define ABS_ADC         ((byte_t) 0x6D)
+#define ABS_AND         ((byte_t) 0x2D)
+#define ABS_ASL         ((byte_t) 0x0E)
+#define ABS_BIT         ((byte_t) 0x2C)
+#define ABS_CMP         ((byte_t) 0xCD)
+#define ABS_CPX         ((byte_t) 0xEC)
+#define ABS_CPY         ((byte_t) 0xCC)
+#define ABS_DEC         ((byte_t) 0xCE)
+#define ABS_EOR         ((byte_t) 0x4D)
+#define ABS_INC         ((byte_t) 0xEE)
+#define ABS_LDA         ((byte_t) 0xAD)
+#define ABS_LDX         ((byte_t) 0xAE)
+#define ABS_LDY         ((byte_t) 0xAC)
+#define ABS_LSR         ((byte_t) 0x4E)
+#define ABS_ORA         ((byte_t) 0x0D)
+#define ABS_ROL         ((byte_t) 0x2E)
+#define ABS_ROR         ((byte_t) 0x6E)
+#define ABS_SBC         ((byte_t) 0xED)
+#define ABS_STA         ((byte_t) 0x8D)
+#define ABS_STX         ((byte_t) 0x8E)
+#define ABS_STY         ((byte_t) 0x8C)
 
-#define BCC             ((byte_t) 0x90)
+#define IMP_BRK         ((byte_t) 0x00)
+#define IMP_CLC         ((byte_t) 0x18)
+#define IMP_CLD         ((byte_t) 0xD8)
+#define IMP_CLI         ((byte_t) 0x58)
+#define IMP_CLV         ((byte_t) 0xB8)
+#define IMP_DEX         ((byte_t) 0xCA)
+#define IMP_DEY         ((byte_t) 0x88)
+#define IMP_INX         ((byte_t) 0xE8)
+#define IMP_INY         ((byte_t) 0xC8)
+#define IMP_NOP         ((byte_t) 0xEA)
+#define IMP_PHA         ((byte_t) 0x48)
+#define IMP_PHP         ((byte_t) 0x08)
+#define IMP_PLA         ((byte_t) 0x68)
+#define IMP_PLP         ((byte_t) 0x28)
+#define IMP_RTI         ((byte_t) 0x40)
+#define IMP_RTS         ((byte_t) 0x60)
+#define IMP_TAX         ((byte_t) 0xAA)
+#define IMP_TAY         ((byte_t) 0xA8)
+#define IMP_TSX         ((byte_t) 0xBA)
+#define IMP_TXA         ((byte_t) 0x8A)
+#define IMP_TXS         ((byte_t) 0x9A)
+#define IMP_TYA         ((byte_t) 0x98)
 
-#define BCS             ((byte_t) 0xB0)
+#define ACC_ASL         ((byte_t) 0x0A)
+#define ACC_LSR         ((byte_t) 0x4A)
+#define ACC_ROL         ((byte_t) 0x2A)
+#define ACC_ROR         ((byte_t) 0x6A)
 
-#define BEQ             ((byte_t) 0xF0)
+#define IXX_ADC         ((byte_t) 0x7D)
+#define IXX_AND         ((byte_t) 0x3D)
+#define IXX_ASL         ((byte_t) 0x1E)
+#define IXX_CMP         ((byte_t) 0xDD)
+#define IXX_DEC         ((byte_t) 0xDE)
+#define IXX_EOR         ((byte_t) 0x5D)
+#define IXX_INC         ((byte_t) 0xFE)
+#define IXX_LDA         ((byte_t) 0xBD)
+#define IXX_LDY         ((byte_t) 0xBC)
+#define IXX_LSR         ((byte_t) 0x5E)
+#define IXX_ORA         ((byte_t) 0x1D)
+#define IXX_ROL         ((byte_t) 0x3E)
+#define IXX_ROR         ((byte_t) 0x7E)
+#define IXX_SBC         ((byte_t) 0xFD)
+#define IXX_STA         ((byte_t) 0x9D)
 
-#define BIT_ZP          ((byte_t) 0x24)
-#define BIT_AB          ((byte_t) 0x2C)
+#define IXY_ADC         ((byte_t) 0x79)
+#define IXY_AND         ((byte_t) 0x39)
+#define IXY_CMP         ((byte_t) 0xD9)
+#define IXY_EOR         ((byte_t) 0x59)
+#define IXY_LDA         ((byte_t) 0xB9)
+#define IXY_LDX         ((byte_t) 0xB6)
+#define IXY_ORA         ((byte_t) 0x19)
+#define IXY_SBC         ((byte_t) 0xF9)
+#define IXY_STA         ((byte_t) 0x99)
 
-#define BMI             ((byte_t) 0x30)
+#define ZPIXX_ADC       ((byte_t) 0x75)
+#define ZPIXX_AND       ((byte_t) 0x35)
+#define ZPIXX_ASL       ((byte_t) 0x16)
+#define ZPIXX_CMP       ((byte_t) 0xD5)
+#define ZPIXX_DEC       ((byte_t) 0xD6)
+#define ZPIXX_EOR       ((byte_t) 0x55)
+#define ZPIXX_INC       ((byte_t) 0xF6)
+#define ZPIXX_LDA       ((byte_t) 0xB5)
+#define ZPIXX_LDY       ((byte_t) 0xB4)
+#define ZPIXX_LSR       ((byte_t) 0x56)
+#define ZPIXX_ORA       ((byte_t) 0x15)
+#define ZPIXX_ROL       ((byte_t) 0x36)
+#define ZPIXX_ROR       ((byte_t) 0x76)
+#define ZPIXX_SBC       ((byte_t) 0xF5)
+#define ZPIXX_STA       ((byte_t) 0x95)
+#define ZPIXX_STY       ((byte_t) 0x94)
 
-#define BNE             ((byte_t) 0xD0)
+#define ZPIXY_LDX       ((byte_t) 0xB6)
+#define ZPIXY_STX       ((byte_t) 0x96)
 
-#define BPL             ((byte_t) 0x10)
+#define IDR_JMP         ((byte_t) 0x6C)
 
-#define BRK             ((byte_t) 0x00)
+#define PREII_ADC       ((byte_t) 0x61)
+#define PREII_AND       ((byte_t) 0x21)
+#define PREII_CMP       ((byte_t) 0xC1)
+#define PREII_EOR       ((byte_t) 0x41)
+#define PREII_LDA       ((byte_t) 0xA1)
+#define PREII_ORA       ((byte_t) 0x01)
+#define PREII_SBC       ((byte_t) 0xE1)
+#define PREII_STA       ((byte_t) 0x81)
 
-#define BVC             ((byte_t) 0x50)
+#define POSII_ADC       ((byte_t) 0x71)
+#define POSII_AND       ((byte_t) 0x31)
+#define POSII_CMP       ((byte_t) 0xD1)
+#define POSII_EOR       ((byte_t) 0x51)
+#define POSII_LDA       ((byte_t) 0xB1)
+#define POSII_ORA       ((byte_t) 0x11)
+#define POSII_SBC       ((byte_t) 0xF1)
+#define POSII_STA       ((byte_t) 0x91)
 
-#define BVS             ((byte_t) 0x70)
-
-#define CLC             ((byte_t) 0x18)
-
-#define CLD             ((byte_t) 0xD8)
-
-#define CLI             ((byte_t) 0x58)
-
-#define CLV             ((byte_t) 0xB8)
-
-#define CMP_IMM         ((byte_t) 0xC9)
-#define CMP_ZP          ((byte_t) 0xC5)
-#define CMP_ZP_X        ((byte_t) 0xD5)
-#define CMP_AB          ((byte_t) 0xCD)
-#define CMP_AB_X        ((byte_t) 0xDD)
-#define CMP_AB_Y        ((byte_t) 0xD9)
-#define CMP_IR_X        ((byte_t) 0xC1)
-#define CMP_IR_Y        ((byte_t) 0xD1)
-
-#define CPX_IMM         ((byte_t) 0xE0)
-#define CPX_ZP          ((byte_t) 0xE4)
-#define CPX_AB          ((byte_t) 0xEC)
-
-#define CPY_IMM         ((byte_t) 0xC0)
-#define CPY_ZP          ((byte_t) 0xC4)
-#define CPY_AB          ((byte_t) 0xCC)
-
-#define DEC_ZP          ((byte_t) 0xC6)
-#define DEC_ZP_X        ((byte_t) 0xD6)
-#define DEC_AB          ((byte_t) 0xCE)
-#define DEC_AB_X        ((byte_t) 0xDE)
-
-#define DEX             ((byte_t) 0xCA)
-
-#define DEY             ((byte_t) 0x88)
-
-#define EOR_IMM         ((byte_t) 0x49)
-#define EOR_ZP          ((byte_t) 0x45)
-#define EOR_ZP_X        ((byte_t) 0x55)
-#define EOR_AB          ((byte_t) 0x4D)
-#define EOR_AB_X        ((byte_t) 0x5D)
-#define EOR_AB_Y        ((byte_t) 0x59)
-#define EOR_IR_X        ((byte_t) 0x41)
-#define EOR_IR_Y        ((byte_t) 0x51)
-
-#define INC_ZP          ((byte_t) 0xE6)
-#define INC_ZP_X        ((byte_t) 0xF6)
-#define INC_AB          ((byte_t) 0xEE)
-#define INC_AB_X        ((byte_t) 0xFE)
-
-#define INX             ((byte_t) 0xE8)
-
-#define INY             ((byte_t) 0xC8)
-
-#define JMP_IR          ((byte_t) 0x6C)
-#define JMP_AB          ((byte_t) 0x4C)
-
-#define JSR             ((byte_t) 0x20)
-
-#define LDA_IMM         ((byte_t) 0xA9)
-#define LDA_ZP          ((byte_t) 0xA5)
-#define LDA_ZP_X        ((byte_t) 0xB5)
-#define LDA_AB          ((byte_t) 0xAD)
-#define LDA_AB_X        ((byte_t) 0xB5)
-#define LDA_AB_Y        ((byte_t) 0xB9)
-#define LDA_IR_X        ((byte_t) 0xA1)
-#define LDA_IR_Y        ((byte_t) 0xB1)
-
-#define LDX_IMM         ((byte_t) 0xA2)
-#define LDX_ZP          ((byte_t) 0xA6)
-#define LDX_ZP_Y        ((byte_t) 0xB6)
-#define LDX_AB          ((byte_t) 0xAE)
-#define LDX_AB_Y        ((byte_t) 0xBE)
-
-#define LDY_IMM         ((byte_t) 0xA0)
-#define LDY_ZP          ((byte_t) 0xA4)
-#define LDY_ZP_X        ((byte_t) 0xB4)
-#define LDY_AB          ((byte_t) 0xAC)
-#define LDY_AB_X        ((byte_t) 0xBC)
-
-#define LSR_AC          ((byte_t) 0x4A)
-#define LSR_ZP          ((byte_t) 0x46)
-#define LSR_ZP_X        ((byte_t) 0x56)
-#define LSR_AB          ((byte_t) 0x4E)
-#define LSR_AB_X        ((byte_t) 0x5E)
-
-#define NOP             ((byte_t) 0xEA)
-
-#define ORA_IMM         ((byte_t) 0x09)
-#define ORA_ZP          ((byte_t) 0x05)
-#define ORA_ZP_X        ((byte_t) 0x15)
-#define ORA_AB          ((byte_t) 0x0D)
-#define ORA_AB_X        ((byte_t) 0x1D)
-#define ORA_AB_Y        ((byte_t) 0x19)
-#define ORA_IR_X        ((byte_t) 0x01)
-#define ORA_IR_Y        ((byte_t) 0x11)
-
-#define PHA             ((byte_t) 0x48)
-
-#define PHP             ((byte_t) 0x08)
-
-#define PLA             ((byte_t) 0x68)
-
-#define PLP             ((byte_t) 0x28)
-
-#define ROL_AC          ((byte_t) 0x2A)
-#define ROL_ZP          ((byte_t) 0x26)
-#define ROL_ZP_X        ((byte_t) 0x36)
-#define ROL_AB          ((byte_t) 0x2E)
-#define ROL_AB_X        ((byte_t) 0x3E)
-
-#define ROR_AC          ((byte_t) 0x6A)
-#define ROR_ZP          ((byte_t) 0x66)
-#define ROR_ZP_X        ((byte_t) 0x76)
-#define ROR_AB          ((byte_t) 0x6E)
-#define ROR_AB_X        ((byte_t) 0x7E)
-
-#define RTI             ((byte_t) 0x40)
-
-#define RTS             ((byte_t) 0x60)
-
-#define SBC_IMM         ((byte_t) 0xE9)
-#define SBC_ZP          ((byte_t) 0xE5)
-#define SBC_ZP_X        ((byte_t) 0xF5)
-#define SBC_AB          ((byte_t) 0xED)
-#define SBC_AB_X        ((byte_t) 0xFD)
-#define SBC_AB_Y        ((byte_t) 0xF9)
-#define SBC_IR_X        ((byte_t) 0xE1)
-#define SBC_IR_Y        ((byte_t) 0xF1)
-
-#define SEC             ((byte_t) 0x38)
-
-#define SED             ((byte_t) 0xF8)
-
-#define SEI             ((byte_t) 0x78)
-
-#define STA_ZP          ((byte_t) 0x85)
-#define STA_ZP_X        ((byte_t) 0x95)
-#define STA_AB          ((byte_t) 0x8D)
-#define STA_AB_X        ((byte_t) 0x9D)
-#define STA_AB_Y        ((byte_t) 0x99)
-#define STA_IR_X        ((byte_t) 0x81)
-#define STA_IR_Y        ((byte_t) 0x91)
-
-#define STX_ZP          ((byte_t) 0x84)
-#define STX_ZP_X        ((byte_t) 0x94)
-#define STX_AB          ((byte_t) 0x8E)
-
-#define STY_ZP          ((byte_t) 0x84)
-#define STY_ZP_X        ((byte_t) 0x94)
-#define STY_AB          ((byte_t) 0x8C)
-
-#define TAX             ((byte_t) 0xAA)
-
-#define TAY             ((byte_t) 0xA8)
-
-#define TSX             ((byte_t) 0xBA)
-
-#define TXA             ((byte_t) 0x8A)
-
-#define TXS             ((byte_t) 0x9A)
-
-#define TYA             ((byte_t) 0x98)
+#define REL_BCC         ((byte_t) 0x90)
+#define REL_BCS         ((byte_t) 0xB0)
+#define REL_BEQ         ((byte_t) 0xF0)
+#define REL_BMI         ((byte_t) 0x30)
+#define REL_BNE         ((byte_t) 0xD0)
+#define REL_BPL         ((byte_t) 0x10)
+#define REL_BVC         ((byte_t) 0x50)
+#define REL_BVS         ((byte_t) 0x70)
 
 
-#define NES_MEMORY_SIZE ((byte_t) 0x10000)
+#define NES_MEMORY_SIZE ((uint32_t) 0x10000)
 
 struct NES
 {
@@ -264,7 +222,7 @@ struct NES
 
     struct CPU cpu;
 
-    
+
 }
 
 /*
